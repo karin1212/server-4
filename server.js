40c685472bd4eeca39144cdd163c56f9e65aa5d2
karin1212 <206808542+karin1212@users.noreply.github.com>
2025-10-24 12:07:24 +0900

    aa

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 // 自分で一から書いてみよう
+import {Hono} from 'jsr:@hono/hono';
+import {serveStatic} from 'jsr:@hono/hono/deno'
+
+const app=new Hono();
+
+app.use('/*',serveStatic({root:'./public'}));
+
+app.get('api', asyno(c) => {
+  const res = await fetch('https://randomuser.me/api/');
+  const data = await res.json();
+
+  return c.json({data});
+});
+
+Deno.serve(app.fetch);
